adde70c9362f9e8a98d796815c9f538124889f10
delta <sumitprasad102@gmail.com>
2025-08-10 16:53:30 +0530

    server upadded link

--- server/server.js
+++ server/server.js
@@ -52,3 +52,10 @@ await connectDB()
 const PORT=process.env.PORT || 5000
 
 server.listen(PORT,()=>console.log("Server is running on PORT: "+PORT))
+
+// if(process.env.NODE_ENV!=="production"){
+    
+// const PORT=process.env.PORT || 5000
+
+// server.listen(PORT,()=>console.log("Server is running on PORT: "+PORT))
+// }
